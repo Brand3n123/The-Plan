c5243112178c775831b3dda57d85b085f82e8877
Brand3n <weeks.branden93@gmail.com>
2025-11-05 12:22:06 -0500

    Update welp_joins_codecademy_project.sql

--- SQL/CodeCademy Courses/6 - Welp Joins/welp_joins_codecademy_project.sql
+++ SQL/CodeCademy Courses/6 - Welp Joins/welp_joins_codecademy_project.sql
@@ -7,25 +7,30 @@
 -- Q4: INNER JOIN (with extra filter)
 SELECT * 
 FROM places
-JOIN reviews ON places.id = reviews.place_id
+JOIN reviews 
+       ON places.id = reviews.place_id
 WHERE total_reviews > 0;
 
 -- Q5: INNER JOIN with selected columns
 SELECT places.name, places.average_rating, 
        reviews.username, reviews.rating, reviews.review_date, reviews.note
 FROM places
-JOIN reviews ON places.id = reviews.place_id
+JOIN reviews 
+       ON places.id = reviews.place_id
 WHERE total_reviews > 0;
 
 -- Q6: LEFT JOIN (but still filtered)
 SELECT places.name, places.average_rating, 
        reviews.username, reviews.rating, reviews.review_date, reviews.note
 FROM places
-LEFT JOIN reviews ON places.id = reviews.place_id
+LEFT JOIN reviews 
+       ON places.id = reviews.place_id
 WHERE total_reviews > 0;
 
 -- Q7: places without reviews
 SELECT places.id, places.name
 FROM places
-LEFT JOIN reviews ON places.id = reviews.place_id
-WHERE reviews.place_id IS NULL;
+LEFT JOIN reviews 
+       ON places.id = reviews.place_id
+WHERE reviews.place_id IS NULL
+ORDER BY places.name;
